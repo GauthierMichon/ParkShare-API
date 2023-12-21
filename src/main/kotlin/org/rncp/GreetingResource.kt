b8fe4e771dbee9762450d8a1cb99fc41534f2d12
GMICHON <gauthier.michon@ynov.com>
2023-12-21 14:36:45 +0100

    Issue 1 : Ping

--- src/main/kotlin/org/rncp/GreetingResource.kt
+++ src/main/kotlin/org/rncp/GreetingResource.kt
@@ -4,6 +4,7 @@ import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
 
 @Path("/hello")
 class GreetingResource {
@@ -11,4 +12,21 @@ class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     fun hello() = "Hello from RESTEasy Reactive"
+}
+
+@Path("/api")
+class PingResource {
+
+    @GET
+    @Path("/ping")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun ping(): Response {
+        val status = "OK"
+        val payload = "{status: 'OK'}"
+
+        return Response
+                .status(if (status == "OK") 200 else 500)
+                .entity(payload)
+                .build()
+    }
 }
